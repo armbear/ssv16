d97d41ee8084561b10b5e13d313e6f681079a594
armbear <55591960+armbear@users.noreply.github.com>
2020-10-30 14:29:25 -0400

    initial commit

---
+++ runbooks/Enable-MFA_v1.6.ps1
@@ -0,0 +1,28 @@
+$cred = Get-AutomationPSCredential -Name "MSOnline"
+Connect-AzureAD -Credential $cred
+Connect-MsolService -Credential $cred
+
+#Enable MFA
+
+$multiFactor = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+$multiFactor.RelyingParty = "*"
+$multiFactor.State = "Enforced"
+$multiFactor.RememberDevicesNotIssuedBefore = (Get-Date) 
+$multiFactorOff = @()
+$domains = Get-MsolDomain
+$secureScoreUser = Get-AutomationVariable -Name "User"
+
+#For all users turn MFA on
+Get-MsolUser | ForEach-Object {
+     Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $multiFactor
+}
+
+#Turn off MFA for the SecureScore user. This is done explicitly in case SecureScore user is not in BreakGlass group yet
+Set-MsolUser -UserPrincipalName $secureScoreUser -StrongAuthenticationRequirements $multiFactorOff 
+
+#If there is a group called "SecureScoreBreakGlass" then exclude the additional users (i.e. Turn MFA Off for them)
+if ((Get-MsolGroup -SearchString "SecureScoreBreakGlass" | Measure-Object).Count -gt 0) {
+  Get-MsolGroupMember -GroupObjectId (Get-MsolGroup -SearchString "SecureScoreBreakGlass").ObjectId | ForEach-Object {
+    Set-MsolUser -UserPrincipalName $_.EmailAddress -StrongAuthenticationRequirements $multiFactorOff
+  }
+}
