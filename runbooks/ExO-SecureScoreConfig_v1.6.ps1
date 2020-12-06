af89abcab629e09eea8ede1a14ebec701358fe88
armbear <55591960+armbear@users.noreply.github.com>
2020-12-06 16:12:04 -0500

    Update ExO-SecureScoreConfig_v1.6.ps1

--- runbooks/ExO-SecureScoreConfig_v1.6.ps1
+++ runbooks/ExO-SecureScoreConfig_v1.6.ps1
@@ -4,6 +4,10 @@ Start-Sleep 20
 $Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid -Credential $cred -Authentication Basic -AllowRedirection
 Import-PSSession -Session $Session -DisableNameChecking:$true -AllowClobber:$true | Out-Null
 Start-Sleep 20
+Write-Output '***Enable-OrganizationCustomization -- Ignore if already hydrated'
+Enable-OrganizationCustomization
+Write-Output '***Enable-OrganizationCustomization Complete'
+Start-Sleep 20
 $GlobalAdminsRoleGroup = Get-MsolRole | ? { $_.Name -eq "Company Administrator" }
 $NotifyOutboundSpamRecipients = (Get-MsolRoleMember -RoleObjectId $GlobalAdminsRoleGroup.ObjectId -MemberObjectTypes User -All).EmailAddress
 $NotifyOutboundSpamRecipients = (Get-MsolCompanyInformation).TechnicalNotificationEmails
